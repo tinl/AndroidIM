337469928b01df693f1fed698fc7ef980e41bdde
管弦 <wangtian7220@163.com>
2017-07-26 23:40:55 +0800

    添加Library

--- app/src/main/java/com/tin/androidim/MainActivity.java
+++ app/src/main/java/com/tin/androidim/MainActivity.java
@@ -1,9 +1,10 @@
 package com.tin.androidim;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tin.library.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
